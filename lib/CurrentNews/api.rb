d554a04742512f171a9027df1a46b512628015ff
Fiona Weaver <fiona.l.weaver@gmail.com>
2020-12-01 10:19:33 -0700

    Done.

--- lib/CurrentNews/api.rb
+++ lib/CurrentNews/api.rb
@@ -5,7 +5,7 @@ require "pry"
 
 class API
 
-    def get_top_articles(country) 
+    def get_articles_by_country(country) 
         key = "d516b0de0724406bba9f3900ae074d27"
         url = "https://newsapi.org/v2/top-headlines?country=#{country}&apiKey=#{key}"
         response = HTTParty.get(url)
@@ -16,4 +16,14 @@ class API
         end 
     end 
 
+    def get_articles_through_catagories(catagory)
+        key = "d516b0de0724406bba9f3900ae074d27"
+        url = "https://newsapi.org/v2/top-headlines?category=#{catagory}&apiKey=#{key}"
+        response = HTTParty.get(url)
+         response['articles'].each do |info|
+             title = info['title']
+             description = info['description']
+             NewsArticles.new(title, description)
+         end
+    end 
 end 
--- lib/CurrentNews/cli.rb
+++ lib/CurrentNews/cli.rb
@@ -1,11 +1,44 @@
+require 'pry'
+
 class CLI 
 
     def start 
-        puts "Welecome to top stories around the world! Which country would you like the top stories on?"
-        puts "Please type the country code:)"
-        list_countries 
+        puts "\nWelecome to top stories around the world! Would you like to see headlines by Country, or Category?\n"
+        main_menu_options
+    end 
+
+    def main_menu_options 
+        print "Please enter either 'Country' or 'Category':"
+        input = gets.chomp
+        if input == "Country" || input == "country"
+           country_it_is
+        elsif input == "Category" || input == "category"
+            categories_it_is
+        elsif input == 'exit'
+            exiting 
+        else 
+            invalid_choice
+            main_menu_options
+        end 
+    end 
+
+    def categories_it_is 
+        list_categories
+        puts "Please enter a category by it's name... (ex. 'health')"
+        print "Please enter a Category: "
         input = gets.chomp 
-        API.new.get_top_articles(input)
+        API.new.get_articles_through_catagories(input)
+        list_articles
+        return_definition
+        continue_or_exit
+    end 
+
+    def country_it_is 
+        list_countries
+        puts "Please enter a country by its country code... (ex. 'US')" 
+        print "Enter Code: "
+        input = gets.chomp 
+        API.new.get_articles_by_country(input)
         list_articles
         return_definition
         continue_or_exit
@@ -16,9 +49,14 @@ class CLI
         country_codes.each.with_index(1) do |country, index|
             puts "#{index}. #{country}"
         end 
-
     end 
- 
+
+    def list_categories 
+        categories = ["business", "entertainment", "health", "sports", "technology"]
+        categories.each.with_index(1) do |categories, index|
+            puts "#{index}. #{categories}"
+        end 
+    end 
 
     def list_articles 
         NewsArticles.all.each.with_index(1) do |info, index|
@@ -26,36 +64,42 @@ class CLI
             puts "#{index}. #{info.title}"
             puts "-------------------------"
         end 
+            puts "\nHere are some titles of top stories, If you would like to see a description on one of these articles, please enter the number of the article you would like to see.\n"
     end 
 
     def return_definition 
-        puts "Here are some titles of top stories in this country, If you would like to see a description on one of these articles, please type the number of the article you would like to see."
+        print "Enter number: "
         input = gets.strip.to_i
         index = input - 1
         article_selected = NewsArticles.all[index]
         puts "-------------------------"
+        sleep(1)
         puts "Description: #{article_selected.description}"
         puts "-------------------------"
     end 
 
+    def invalid_choice 
+        puts "Sorry! That was an invalid choice. Please try again."
+    end 
+    
+    def exiting 
+        puts "\nExiting...\n"
+        puts "Thanks for coming!"
+    end 
 
     def continue_or_exit
-        puts "To see top stories from a different country, please type 'main menu'."
+        puts "\nTo see top stories from a different country, or category, please type 'main menu'.\nexit"
         puts "If you would like to exit, please type 'exit'."
         input = gets.strip 
-        if input == "back"
-            return_definition
-        elsif input == "main menu"
-            start
+        if input == "main menu"
+            main_menu_options
         elsif input == "exit"
-            puts "Exiting, Thanks for coming!"
-            exit 
+            exiting 
+        else 
+            invalid_choice
+            continue_or_exit
         end 
             
     end 
-        
-
-
-
 
 end 
